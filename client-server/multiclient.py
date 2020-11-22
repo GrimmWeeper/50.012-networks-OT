30ab92c486e2f84e2f0e7506e49f2b9bcab9bf9c
Alice <jingyu.zhang@flex-solver.com.sg>
2020-11-22 14:59:24 +0800

    perform multiclient and enter numbers from 1 - 5 to see best results

--- client-server/multiclient.py
+++ client-server/multiclient.py
@@ -1,7 +1,7 @@
-#import the socket module
+# import the socket module
 import socket
 
-#Create a socket instance
+# Create a socket instance
 socketObject = socket.socket()
 host = '127.0.0.1'
 port = 2004
@@ -23,32 +23,34 @@ for i in range(clientNumber):
         print(str(e))
 
     res = socketObject.recv(1024)
-    if(i == 0): 
+    # Writing something
+    if(i == 0):
         message = "Test message from Client {} \n".format(i+1)
-        f = open("sharedfile.txt","w")
+        f = open("sharedfile.txt", "w")
         f.write(message)
         f.close()
         messageBytes = str.encode(message)
         socketObject.sendall(messageBytes)
         data = socketObject.recv(1024)
         print(data)
-    elif(i==1):
+    #Adding on to client 1
+    elif(i == 1):
         message = "Test message from other client {} \n".format(i+1)
-        f = open("sharedfile.txt","a")
+        f = open("sharedfile.txt", "a")
         f.write(message)
         f.close()
         messageBytes = str.encode(message)
         socketObject.sendall(messageBytes)
         data = socketObject.recv(1024)
         print(data)
-    elif(i == 2): 
-        message = "I've deleted edits made by client 1"
-        open_file = open("sharedfile.txt","r")
+    #Delete
+    elif(i == 2):
+        message = "Client 3: I've deleted edits made by client 2 \n"
+        open_file = open("sharedfile.txt", "r")
         lines = open_file.readlines()
-        print(lines)
         open_file.close()
         del lines[1]
-        f = open("sharedfile.txt","w+")
+        f = open("sharedfile.txt", "w+")
         for line in lines:
             f.write(line)
         f.write(message)
@@ -57,6 +59,40 @@ for i in range(clientNumber):
         socketObject.sendall(messageBytes)
         data = socketObject.recv(1024)
         print(data)
-    
+    #Insert 
+    elif(i == 3):
+        open_file = open("sharedfile.txt", "r")
+        lines = open_file.readlines()
+        open_file.close()
+        message = "Client 4 has inserted this message between the first two lines \n"
+        lines.insert(1,message)
+        f = open("sharedfile.txt","w+")
+        for line in lines:
+            f.write(line)
+        f.close()
+        messageBytes = str.encode(message)
+        socketObject.sendall(messageBytes)
+        data = socketObject.recv(1024)
+        print(data)
+    #Move
+    else: 
+        open_file = open("sharedfile.txt","r")
+        lines = open_file.readlines()
+        open_file.close()
+        first_line = lines[0]
+        lines.pop(0)
+        lines.append(first_line)
+        f = open("sharedfile.txt","w+")
+        for line in lines:
+            f.write(line)
+        message = "Client 5 has moved the edit made by client 1 \n"
+        f.write(message)
+        f.close()
+        messageBytes = str.encode(message)
+        socketObject.sendall(messageBytes)
+        data = socketObject.recv(1024)
+        print(data)
+
+
 
 socketObject.close()

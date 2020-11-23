17b426ef60702a73bab31233feb704c8669a08c3
Alice <jingyu.zhang@flex-solver.com.sg>
2020-11-24 00:33:28 +0800

    tolerate server loss

--- client-server/client.py
+++ client-server/client.py
@@ -1,5 +1,6 @@
 #import the socket module
 import socket
+import random
 
 #Create a socket instance
 socketObject = socket.socket()
@@ -17,9 +18,21 @@ except socket.error as e:
     print(str(e))
 
 res = socketObject.recv(1024)
+waitingForServerToComeback = True
+unsentBuffer = []
+secondSocket = socket.socket()
 
 # Receive the data
 while(True):
+    if len(unsentBuffer) > 0: 
+        print("Re-sending lost messages to server")
+        for i in range(len(unsentBuffer)):
+            messageBytes = str.encode(unsentBuffer[i])
+            secondSocket.sendall(messageBytes)
+            res = secondSocket.recv(1024)
+            print("Re-sending lost messages to server")
+            print(res)
+        unsentBuffer = []
 
     message = input("Input message to send to server: ")
 
@@ -27,11 +40,40 @@ while(True):
         break
     
     else:
-        messageBytes = str.encode(message)
-        socketObject.sendall(messageBytes)
+        try:
+            if waitingForServerToComeback == True:
+                messageBytes = str.encode(message)
+                socketObject.sendall(messageBytes)
+            else:
+                messageBytes = str.encode(message)
+                secondSocket.sendall(messageBytes)
 
-    data = socketObject.recv(1024)
-    print(data)
+
+        except ConnectionResetError as e: #server connection shut down
+            unsentBuffer.append(message) #place the unsent message in the buffer
+            
+            while(waitingForServerToComeback):
+                
+                try:
+                    #newPort = random.randint(port+1,8000)
+                    #port_number = newPort
+                    #newPort = socket.socket() #new socket used to try and reconnect
+                    secondSocket.connect((host,port))
+                    print("Server is back")
+                    waitingForServerToComeback = False
+                    
+
+                except ConnectionRefusedError:
+                    print(str(e))
+
+        
+    if waitingForServerToComeback == True:
+        data = socketObject.recv(1024)
+        print(data)
+    else:
+        data = secondSocket.recv(1024)
+        print(data)
 
 
 socketObject.close()
+secondSocket.close()
